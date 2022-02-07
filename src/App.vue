3cc28c7b21b2720e653545d2d291bc12c45dcbf2
Mark-Inholland <mark.dehaan@inholland.nl>
2022-02-07 21:36:28 +0100

    Basic routing example added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello App!</h1>
+  <nav>
+    <router-link to="/">Go to Home</router-link> - 
+    <router-link to="/about">Go to About</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+];
+
+const router = createRouter({
+    "history": createWebHistory(),
+    routes
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
